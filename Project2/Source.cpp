49133c080d567b9ac107cc91d3fc375b9b8cb01b
ruslan savelev <carry2002life@mail.ru>
2021-10-08 22:46:03 +0300

    commit2

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 template<typename T>
 class List
@@ -7,11 +8,13 @@ public:
 	List()
 	{
 		this->list_length = 0;
+		first_element_ = this;
 	}
 	List(const T& value)
 	{
 		this->value_ = value;
 		this->last_element_ = this;
+		this->first_element_ = this;
 		list_length = 1;
 	}
 	void list_push_back(const T& value)
@@ -19,8 +22,8 @@ public:
 		if (list_length == 0)
 		{
 			this->value_ = value;
-			this->prev_element_ = this;
 			this->last_element_ = this;
+			this->first_element_ = this;
 		}
 		else
 		{
@@ -34,31 +37,32 @@ public:
 	{
 		if (list_length == 0)
 		{
-			return;
+			throw string("exception no element");
 		}
 		if (list_length == 1)
 		{
 			this->last_element_ = this;
-			prev_element_ = this;
+			this->first_element_ = this;
+			delete last_element_;
 			list_length--;
 			return;
 		}
-		this->last_element_ = prev_element_;
-		prev_element_ = last_element_->prev_element_;
-		delete last_element_->next_;
+		delete last_element_;
+		last_element_ = prev_element_;
 		list_length--;
 	}
 	void list_push_front(const T& value)
 	{
-		if (list_length == 0)
-			this->value_ = value;
-		else
-		{
-			List* new_list = this;
-			this->value_ = value;
-			this->next_ = new_list;
-		}
+		List* new_ = new List(value);
 		list_length++;
+		if (list_length == 1)
+		{
+			new_->next_ = nullptr;
+			first_element_ = last_element_ = new_;
+			return;
+		}
+		new_->next_ = first_element_;
+		first_element_ = new_;
 	}
 	T& get_value()
 	{
@@ -70,14 +74,11 @@ public:
 	{
 		return *(this->next_);
 	}
-	List& get_prev()
-	{
-		return *(this->prev_element_);
-	}
 	List& operator=(const List& list)
 	{
 		this->value_ = list.value;
 		this->next_ = list.next_;
+		this->first_element_ = list.first_element_;
 		return *this;
 	}
 	int get_list_length()
@@ -86,22 +87,25 @@ public:
 	}
 	T& operator[](int index)
 	{
-		return func(this, 0, index);
+		return func(first_element_, 0, index);
 	}
 	T& func(List<T>* list, int i, int index)
 	{
+		if (!list)
+			throw string("exception no index access");
 		if (i > index || index >= this->list_length)
 			throw string("exception no index access");
 		if (i == index)
 			return list->get_value();
 		func(list->next_, ++i, index);
-		
+
 	}
 private:
 	T value_; //value of type T
 	List* last_element_; //pointer to the last element
 	List* prev_element_; //poiter to element before the last one
 	List* next_; //pointer to the next element of the List
+	List* first_element_; //pointer to first element of the List
 	int list_length; // length of the List
 };
 
@@ -111,18 +115,23 @@ int main()
 {
 	try
 	{
-		List<int> l1;
-		int n = 0;
-		cin >> n;
-		for (int i = 0; i < n; i++)
+		List<int> list;
+		int N = 0;
+		cin >> N;
+		for (int i = 0; i < N; i++)
 		{
-			int value;
+			int value = 0;
 			cin >> value;
-			l1.list_push_back(value);
+			list.list_push_back(value);
 		}
-		int m = 0;
-		cin >> m;
-		cout << l1[m];
+		list.list_pop_back();
+		list.list_pop_back();
+		list.list_push_front(-1);
+		list.list_push_front(-2);
+		for (int i = 0; i < list.get_list_length(); i++)
+		{
+			cout << list[i] << " ";
+		}
 	}
 	catch (string exception)
 	{
